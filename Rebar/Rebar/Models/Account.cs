175a1693fc5e0b5196f2af54e4be4dbf4919b81b
Sara-Farkash <Sarifark4@gmail.com>
2023-10-30 11:28:25 +0200

    Creating the first layer of the model layer project The departments created: Account Order DiscountsAndPromotions Menu Shake

---
+++ Rebar/Rebar/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace Rebar.Models
+{
+    public class Account
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+
+        [BsonElement("lumpSum")]
+        public double LumpSum { get; set; }
+
+        [BsonElement("listOrders")]
+        public List<Order> ListOrders { get; set; }
+    }
+}
---
+++ Rebar/Rebar/Models/Shake.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace Rebar.Models
+{
+    public enum EnumPriceForSize
+    {
+        L=32,M=28,S=23
+    }
+    public class Shake
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("priceForSize")]
+        public EnumPriceForSize PriceForSize { get; set; }
+
+
+
+
+    }
+}
